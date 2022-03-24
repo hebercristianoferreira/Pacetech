a5645524305f86e8d927d148e9b128a27a986ed2
hebercristianoferreira <100002012+hebercristianoferreira@users.noreply.github.com>
2022-03-24 09:24:03 -0300

    Aprendizagem

---
+++ semana7/Pessoa.java
@@ -0,0 +1,31 @@
+
+package semana7;
+
+
+public class Pessoa {
+    
+    public float peso;
+    public float altura;
+    
+    public Pessoa (float peso, float altura){
+        this.peso = peso;
+        this.altura = altura;    
+    }   
+    public float calcularIMC(){
+        float imc = peso / (altura * altura);
+        return imc;
+    } 
+    public void setPeso(float peso){
+        this.peso= peso;
+    }
+    public float getPeso(){
+        return peso;
+    } 
+    public void setAltura(float altura){
+        this.altura= altura;
+    }  
+    public float getAltura(){
+        return altura;
+    }
+    
+}
---
+++ semana7/Semana7.java
@@ -0,0 +1,28 @@
+
+package semana7;
+
+import java.util.Scanner;
+
+public class Semana7 {
+
+    
+    public static void main(String[] args) {
+       
+        
+       Pessoa objetoPessoa = new Pessoa(70.0f , 1.70f);
+     
+       
+       Scanner leitor = new Scanner(System.in);
+       
+        System.out.println(" Digite o peso da pessoa");
+        objetoPessoa.setPeso(leitor.nextFloat());
+        System.out.println(" Digite a altura da pessoa");
+        objetoPessoa.setAltura(leitor.nextFloat());
+        
+        
+        
+        System.out.println(" IMC= "+objetoPessoa.calcularIMC());
+        
+    }
+    
+}
